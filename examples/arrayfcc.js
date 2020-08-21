0eae7878a132c22387fec28e6f46cb42603ad75c
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-08-21 13:25:14 +0600

    freecodecamp javascript array

---
+++ examples/arrayfcc.js
@@ -0,0 +1,51 @@
+// Only change code below this line
+var myArray = ['rony', 8];
+
+// Only change code below this line
+var myArray = [[2, 3], ['rony', 'sattar']];
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line
+var myData = myArray[0];
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line
+myArray[0] = 45;
+
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line
+var myData = myArray[2][1];
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray.push(['dog', 3]);
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+var removedFromMyArray = myArray.pop(['cat', 2]);
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+var removedFromMyArray = myArray.shift();
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(['Paul', 35]);
+
+var myList = [['peanut', 4], ['what', 4], ['jackfruit', 4], ['orange', 4], ['mango', 4], ['chocolate', 4]];
+
